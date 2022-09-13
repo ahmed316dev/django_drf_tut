774de3da0117cd594b9c888053ed335e73dbbcfe
influ <26268158+ahmed316dev@users.noreply.github.com>
2022-09-13 14:13:23 +0300

    created Products model

---
+++ backend/products/models.py
@@ -0,0 +1,8 @@
+from importlib.resources import contents
+from turtle import title
+from django.db import models
+
+class Products(models.Model):
+    title = models.CharField(max_length=120)
+    content = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=15, decimal_places=2, default=99.99)
